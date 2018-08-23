3599037a18e1cd63a45d3962eef6a11e55643196
Akhila <akhi.prabha@gmail.com>
2018-08-23 17:29:46 +0530

    dynamic component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DynamicComponentComponent } from './dynamic-form/dynamic-component/dynamic-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DynamicComponentComponent
   ],
   imports: [
     BrowserModule
